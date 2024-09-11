23f21bbaeae354c37b5bbfd5c75ee0deef6de0c5
alifbinhossain <alifahmed47@gmail.com>
2024-09-11 18:33:32 +0600

    ci: :sparkles: added shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,12 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@src': resolve(__dirname, './src'),
-      '@assets': resolve(__dirname, './src/assets'),
-      '@hooks': resolve(__dirname, './src/hooks'),
-      '@pages': resolve(__dirname, './src/pages'),
-      '@components': resolve(__dirname, './src/components'),
-      '@utils': resolve(__dirname, './src/utils'),
-      '@libs': resolve(__dirname, './src/libs'),
-      '@ui': resolve(__dirname, './src/ui'),
-      '@context': resolve(__dirname, './src/context'),
-      '@layouts': resolve(__dirname, './src/layouts'),
-      '@routes': resolve(__dirname, './src/routes'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
