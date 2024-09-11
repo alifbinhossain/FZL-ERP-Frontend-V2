233d1c58efaf764f4cfdd8b93342b66987d9f2bf
alifbinhossain <alifahmed47@gmail.com>
2024-09-11 18:15:47 +0600

    ci: :sparkles: added absolute import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@src': resolve(__dirname, './src'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@components': resolve(__dirname, './src/components'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@libs': resolve(__dirname, './src/libs'),
+      '@ui': resolve(__dirname, './src/ui'),
+      '@context': resolve(__dirname, './src/context'),
+      '@layouts': resolve(__dirname, './src/layouts'),
+      '@routes': resolve(__dirname, './src/routes'),
+    },
+  },
   plugins: [react()],
   server: {
     port: 3000,
