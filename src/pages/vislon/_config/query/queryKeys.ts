936a741bba821b5df38cbf387267c2c823e7f8b7
alifbinhossain <alifahmed47@gmail.com>
2024-10-15 12:53:19 +0600

    ci: :sparkles: added vislon schema and queries

---
+++ src/pages/vislon/_config/query/queryKeys.ts
@@ -0,0 +1,109 @@
+export const vislonQK = {
+	all: () => ['vislon'],
+
+	//* Teeth Molding
+	// * RM
+	VislonTMRM: () => [...vislonQK.all(), 'tm-rm'],
+	VislonTMRMByUUID: (uuid: string) => [...vislonQK.VislonTMRM(), uuid],
+
+	// * RM Log
+	VislonTMRMLog: () => [...vislonQK.all(), 'tm-rm-log'],
+	VislonTMRMLogByUUID: (uuid: string) => [...vislonQK.VislonTMRMLog(), uuid],
+
+	// * Order Against vislonTMRMLog * //// * Order Against vislonTM RM Log * //
+	orderAgainstVislonTMRMLog: () => [
+		...vislonQK.all(),
+		'vislon-tm',
+		'order-against-rm-log',
+	],
+	orderAgainstVislonTMRMLogByUUID: (uuid: string) => [
+		...vislonQK.orderAgainstVislonTMRMLog(),
+		uuid,
+	],
+
+	// * Vislon Teeth Molding Production combined data
+	vislonTMP: () => [...vislonQK.all(), 'vislonTMP'],
+	vislonTMPByUUID: (uuid: string) => [...vislonQK.vislonTMP(), uuid],
+
+	// * Vislon Teeth Molding Transaction combined data
+	vislonTMT: () => [...vislonQK.all(), 'vislonTMT'],
+	vislonTMTByUUID: (uuid: string) => [...vislonQK.vislonTMT(), uuid],
+
+	// * Vislon Teeth Molding Production Entry
+	vislonTMPEntry: () => [...vislonQK.all(), 'vislonTMPEntry'],
+	vislonTMPEntryByUUID: (uuid: string) => [
+		...vislonQK.vislonTMPEntry(),
+		uuid,
+	],
+
+	// * Vislon Teeth Molding Transaction Entry
+	vislonTMTEntry: () => [...vislonQK.all(), 'vislonTMTEntry'],
+	vislonTMTEntryByUUID: (uuid: string) => [
+		...vislonQK.vislonTMTEntry(),
+		uuid,
+	],
+
+	// * Vislon Teeth Molding Production Log
+	vislonTMPLog: () => [...vislonQK.all(), 'vislonTMPLog'],
+	vislonTMPLogByUUID: (uuid: string) => [...vislonQK.vislonTMPLog(), uuid],
+
+	// * Vislon Teeth Molding Transaction Log
+	vislonTMTLog: () => [...vislonQK.all(), 'vislonTMTLog'],
+	vislonTMTLogByUUID: (uuid: string) => [...vislonQK.vislonTMTLog(), uuid],
+
+	//* Vislon Teeth Molding Tape Log
+	vislonTMTapeLog: () => [...vislonQK.all(), 'vislonTMTapeLog'],
+	vislonTMTapeLogByUUID: (uuid: string) => [
+		...vislonQK.vislonTMTapeLog(),
+		uuid,
+	],
+
+	// * Finishing
+	//*RM
+	VislonFinishingRM: () => [...vislonQK.all(), 'fin-rm'],
+	VislonFinishingRMByUUID: (uuid: string) => [
+		...vislonQK.VislonFinishingRM(),
+		'rm',
+		uuid,
+	],
+
+	//*RM Log
+	VislonFinishingRMLog: () => [...vislonQK.all(), 'fin-rm-log'],
+	VislonFinishingRMLogByUUID: (uuid: string) => [
+		...vislonQK.VislonFinishingRMLog(),
+		'rm-log',
+		uuid,
+	],
+
+	// * Order Against vislonFinishing RM Log * //
+	orderAgainstVislonFinishingRMLog: () => [
+		...vislonQK.all(),
+		'vislon-finishing',
+		'order-against-rm-log',
+	],
+	orderAgainstVislonFinishingRMLogByUUID: (uuid: string) => [
+		...vislonQK.orderAgainstVislonFinishingRMLog(),
+		uuid,
+	],
+
+	// * finishing  Production & Transaction combined data
+	vislonFinishingProd: () => [...vislonQK.all(), 'vislon-finishing', 'prod'],
+	vislonFinishingProdByUUID: (uuid: string) => [
+		...vislonQK.vislonFinishingProd(),
+		uuid,
+	],
+
+	// * Finishing Production Log
+	vislonFinishingProdLog: () => [
+		...vislonQK.all(),
+		'vislon-finishing',
+		'prod-log',
+	],
+
+	// * Finishing Transaction Log
+	vislonFinishingTrxLog: () => [
+		...vislonQK.all(),
+		'vislon-finishing',
+		'trx-log',
+	],
+};
---
+++ src/pages/vislon/_config/schema/index.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+import { NUMBER_DOUBLE_REQUIRED, STRING_NULLABLE } from '@/utils/validators';
+
+// Vislon Transaction Schema
+export const VISLON_TRANSACTION_SCHEMA = z.object({
+	trx_quantity_in_kg: NUMBER_DOUBLE_REQUIRED,
+	remarks: STRING_NULLABLE,
+});
+
+export const VISLON_TRANSACTION_SCHEMA_NULL: Partial<IVislonTransactionSchema> =
+	{
+		remarks: null,
+	};
+
+export type IVislonTransactionSchema = z.infer<
+	typeof VISLON_TRANSACTION_SCHEMA
+>;
