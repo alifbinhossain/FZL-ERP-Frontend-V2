8b156a64468b6da6b2a9761cdc5214ad00403034
Md. Shahadat Anik Sheikh <git.shahadat.anik@gmail.com>
2024-11-07 19:01:42 +0600

    fix: :art: CHANGED config

--- eslint.config.js
+++ eslint.config.js
@@ -9,11 +9,15 @@ export default tseslint.config(
 		ignores: ['dist', 'src/components/ui'],
 	},
 	{
-		extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+		extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
 		files: ['**/*.{ts,tsx}'],
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
+			parserOptions: {
+				project: ['./tsconfig.node.json', './tsconfig.app.json'],
+				tsconfigRootDir: import.meta.dirname,
+			},
 		},
 		plugins: {
 			'react-hooks': reactHooks,
